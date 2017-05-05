65fb4a6ff24bb971c44550df82e13ac31719b4f5
Fernando Vilas Maciel <fvilas@gradiant.org>
2017-05-05 13:18:23 +0200

    Initial commit

---
+++ src/test/java/FizzBuzzTest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class FizzBuzzTest {
+
+	@Test
+	public void renameMe() {
+		assertTrue(true);
+	}
+
+}
